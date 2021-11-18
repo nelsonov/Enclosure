2344b3fffe886b737032ea57dd3fe16b306811f5
Lee Nelson <lnelson@nelnet.org>
2021-11-17 19:52:33 -0800

    Expermenting

--- src/enclosure.cpp
+++ src/enclosure.cpp
@@ -4,8 +4,6 @@
 #include <Adafruit_NeoPixel.h>
 #include <DHT.h>
 #include "pins.h"
-#define PIXEL_PIN 10
-#define PIXEL_COUNT 60
 
 //If we are in arvdebug environment, then include necessary headers
 //and define DEBUGMSG macro to use avr-stub's debug_message since
@@ -20,21 +18,18 @@
 #define DEBUGMSG(m) Serial.print(m)
 #endif
 #define BOARD_LED 13
-
-//If we are using an RGBW strip, then set PIXELCOLOR macro to use
-//all args and create the proper object for RGBW
+volatile int pixcount = 30;
 #ifdef STRIP_RGBW
-#define PIXELCOLOR(n, r, g, b, w) strip.setPixelColor(n, r, g, b, w);
-//Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN, NEO_RGBW + NEO_KHZ800);
-//IF using a "normal" RGB strip, then the PIXELCOLOR macro to
-//ignore the 5th (white) argument. Also create an object for RGB
+Adafruit_NeoPixel lightPixels(NEOPIXEL1_COUNT, NEOPIXEL1, NEO_RGBW + NEO_KHZ800);
 #else
-#define PIXELCOLOR(n, r, g, b, w) strip.setPixelColor(n, r, g, b);
-Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN);
+Adafruit_NeoPixel lightPixels(NEOPIXEL1_COUNT, NEOPIXEL1);
 #endif
+Adafruit_NeoPixel statusPixels(NEOPIXEL2_COUNT, NEOPIXEL2);
 
+uint32_t cold=lightPixels.Color(0,0,128,0);
 bool justBooted = true;
-volatile int delaytime = 500;
+volatile int delaytime = 100;
+volatile int bytecolor=1;
 void setup () {
 #ifdef AVRDEBUG
     debug_init();
@@ -42,8 +37,10 @@ void setup () {
 #ifndef AVRDEBUG
     Serial.begin(9600);
 #endif
-    strip.begin();
-    strip.show(); //All pixels off
+    lightPixels.begin();
+    lightPixels.show(); //All pixels off
+    statusPixels.begin();
+    statusPixels.show(); //All pixels off
     pinMode (BOARD_LED, OUTPUT);
 };
 void loop () {
@@ -51,15 +48,18 @@ void loop () {
         justBooted = false;
         DEBUGMSG("Starting\n");
     }
-    //strip.setPixelColor(6, 0, 0, 0, 128);
-    //strip.setPixelColor(8, 0, 0, 128, 0);
-    //strip.setPixelColor(10, 0, 128, 0, 0);
-    //strip.setPixelColor(12, 128, 0, 0, 0);
-    PIXELCOLOR(6, 0, 0, 0, 128);
-    PIXELCOLOR(8, 0, 0, 128, 0);
-    PIXELCOLOR(10, 0, 128, 0, 0);
-    PIXELCOLOR(12, 128, 0, 0, 0);
-    strip.show();
+    lightPixels.fill(cold, 1, pixcount);
+//    lightPixels.setPixelColor(1, 0, 0, 0, 128);
+//    lightPixels.setPixelColor(2, 0, 0, bytecolor, 0);
+//    lightPixels.setPixelColor(3, 0, bytecolor, 0, 0);
+//    lightPixels.setPixelColor(4, bytecolor, 0, 0, 0);
+    statusPixels.setPixelColor(2, 0, 0, bytecolor);
+    statusPixels.setPixelColor(3, 0, bytecolor, 0);
+    statusPixels.setPixelColor(4, bytecolor, 0, 0);
+    bytecolor=(bytecolor + 1) % 128;
+    //DEBUGMSG(char(bytecolor));
+    lightPixels.show();
+    statusPixels.show();
     digitalWrite (BOARD_LED, HIGH);
     DEBUGMSG("On\n");
     delay (delaytime);
--- src/pins.h
+++ src/pins.h
@@ -1,5 +1,6 @@
 #define OCTO_PRINTERPWR     15 // RPi GPIO22
 #define OCTO_CANCEL         13 // RPi GPIO27
+#define OCTO_STEPPERS       29 // RPi GPIO5
 #define LIGHTENCODERA       36 // 2560
 #define LIGHTENCODERB       37 // 2560
 #define LIGHTENCODERSW      30 // 2560
@@ -11,6 +12,11 @@
 #define CAM04_PWR           51 // 2560
 #define BEDFAN_SW           34 // 2560
 #define BEDFAN_PWR          46 // 2560
+#define DHT22               44 // 2560
+#define NEOPIXEL1           52 // 2560
 #define NEOPIXEL2           31 // 2560
 #define AUXPWR              50 // 2560
 #define STATDEBUG           11 // ESP32 GPIO 27
+
+#define NEOPIXEL1_COUNT 8
+#define NEOPIXEL2_COUNT 8
